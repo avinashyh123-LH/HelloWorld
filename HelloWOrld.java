b1b65b16713186937bc23fe37cafd1746a0f0ee0
avinashyh123-LH <60501096+avinashyh123-LH@users.noreply.github.com>
2020-01-31 15:35:53 +0530

    Add files via upload

---
+++ HelloWOrld.java
@@ -0,0 +1,12 @@
+package MavenDemo;
+
+public class HelloWOrld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Avinash");
+		System.out.println("Hello AVin");
+
+	}
+
+}
